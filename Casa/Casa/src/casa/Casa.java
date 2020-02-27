40fa8ce1f6209737d3232384ae5ba1858d8506ee
cossmendioladianakaren <60638076+cossmendioladianakaren@users.noreply.github.com>
2020-02-26 22:07:44 -0600

    Add files via upload

---
+++ Casa/Casa/src/casa/Casa.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package casa;
+
+/**
+ *
+ * @author Coss Mendiola Diana Karen
+ */
+public class Casa {
+
+    private int puertas;
+    private int pisos;
+    private int television;
+    private int muebles;
+
+    public Casa(int puertas, int pisos, int television, int muebles) {
+        this.puertas = puertas;
+        this.pisos = pisos;
+        this.television = television;
+        this.muebles = muebles;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    public int getPuertas() {
+        return puertas;
+    }
+
+    public void setPuertas(int puertas) {
+        this.puertas = puertas;
+        System.out.println("Abrir puertas");
+        System.out.println("Cerrar puertas");
+    }
+
+    public int getPisos() {
+        return pisos;
+    }
+
+    public void setPisos(int pisos) {
+        this.pisos = pisos;
+    }
+
+    public int getTelevision() {
+        return television;
+    }
+
+    public void setTelevision(int television) {
+        this.television = television;
+        System.out.println("Encendiendo la television");
+    }
+
+    public int getMuebles() {
+        return muebles;
+    }
+
+    public void setMuebles(int muebles) {
+        this.muebles = muebles;
+    }
+    
+}
